b2c7584792cf887d1aedac7f114de50b15a5adba
Fozle Rabbi Shafi <frshafi49@gmail.com>
2022-05-16 18:20:57 +0600

    updated card porops

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import Card from "./components/Card"
 import Hero from "./components/Hero"
 import NavBar from "./components/Navbar"
 
+import cardData from './data';
+
 export default function App() {
+
+  const cards = cardData.map(item => {
+    return (
+      <Card
+        key={item.id}
+        item={item}
+      />
+
+    )
+  })
   return (
     <>
-      {/* <NavBar />
-      <Hero /> */}
-      <Card
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6}
-        country="BD"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      <NavBar />
+      <Hero />
+      <div className="cards-container">
+        {cards}
+      </div>
     </>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,29 @@
-import katieImage from '../images/katie-zaferes.png'
 import star from '../images/star.png';
-
 export default function Card(props) {
-    console.log(props)
+
+    console.log('props.item', props.item);
+
+    console.log(props.item.openSpots)
+    console.log(props.item.location)
+
+
+    let badgeText;
+    if (props.item.openSpots === 0) badgeText = 'SOLD OUT';
+    else if (props.item.location === "Online") badgeText = "ONLINE";
+
     return (
         <div className="card-container">
-
-            <img src={katieImage} alt="katie-zaferes" className='card-thumbnail'/>
+            {badgeText && <div className='card-badge'>{badgeText}</div>}
+            <img src={require(`../images/${props.item.coverImg}`)} alt="katie-zaferes" className='card-thumbnail' />
             <div className="rating">
                 <img src={star} alt="star" className='star' />
-                <span className='rating-point' >{props.rating}</span> 
-                <span className='rating-people'>{props.reviewCount} • </span>
-                <span className='rating-country'>{props.country}</span>
+                <span className='rating-point' >{props.item.stats.rating}</span>
+                <span className='rating-people'>{props.item.stats.reviewCount} • </span>
+                <span className='rating-country'>{props.item.location}</span>
             </div>
-            <p className='card-title'>{props.title}</p>
+            <p className='card-title'>{props.item.title}</p>
             <div className='card-fee'>
-                <p className='fee'>From ${props.price}</p>
+                <p className='fee'>From ${props.item.price}</p>
                 <p className='fee-slash'>/</p>
                 <p>person</p>
             </div>
